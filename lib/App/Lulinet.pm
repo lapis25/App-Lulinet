9223f6e291a903141a86bc6a499023e6b93c4722
lapis25 <lapis25@gmail.com>
2011-01-22 09:31:20 +0900

    initial commit

---
+++ lib/App/Lulinet.pm
@@ -0,0 +1,32 @@
+package App::Lulinet;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+App::Lulinet -
+
+=head1 SYNOPSIS
+
+  use App::Lulinet;
+
+=head1 DESCRIPTION
+
+App::Lulinet is
+
+=head1 AUTHOR
+
+lapis25 E<lt>lapis25@gmail.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
